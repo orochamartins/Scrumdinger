555dcd18172c59e708ae5190e2a8794f23a91218
Renato Martins <rochamartinsrenato@gmail.com>
2023-11-20 08:49:24 +0000

    Next: Section 1 - Create the meeting timer view

---
+++ Scrumdinger/History.swift
@@ -0,0 +1,20 @@
+//
+//  History.swift
+//  Scrumdinger
+//
+//  Created by Renato Martins on 16/11/2023.
+//
+
+import Foundation
+
+struct History: Identifiable, Codable {
+    let id: UUID
+    let date: Date
+    var attendees: [DailyScrum.Attendee]
+    
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee]) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+    }
+}
